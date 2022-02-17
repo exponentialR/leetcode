430ab030dbc2d6de9f2cf45c9cd2b046de2b331d
exponentialR <soluadebayo@gmail.com>
2022-02-17 13:55:04 +0000

    day 3 updated

---
+++ containsDuplicate.py
@@ -0,0 +1,29 @@
+"""Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+"""
+
+
+def containsDuplicate(nums):
+    return len(set(nums)) != len(nums)
+
+#~~~~~~~Testing~~~~~~~~~~
+nums = [1,2,3,1]
+nums2 = [1,2,3,4]
+print(containsDuplicate(nums))
+print(containsDuplicate(nums2))
